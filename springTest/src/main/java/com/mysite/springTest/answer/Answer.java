8c3d6ede743ecf5077eb14ea03cd07c6a77131f7
junghun03231 <052002@naver.com>
2023-11-21 13:48:42 +0900

    springTest

---
+++ springTest/src/main/java/com/mysite/springTest/answer/Answer.java
@@ -0,0 +1,28 @@
+package com.mysite.springTest.answer;
+
+
+import java.time.LocalDateTime;
+
+import com.mysite.springTest.questoin.Question;
+import jakarta.persistence.*;
+
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
---
+++ springTest/src/main/java/com/mysite/springTest/questoin/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.mysite.springTest.questoin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
